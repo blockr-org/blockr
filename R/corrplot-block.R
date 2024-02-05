0ec343e5455add960c2c41f7c97265fa35cad0c9
Dhadage <ramnathbalasaheb.dhadage@bms.com>
2024-02-05 12:49:13 +0530

    Fix correlation plot issues with non-numeric and missing values - Filter out non-numeric values from the data before plotting - Drop columns that are completely missing and rows that contain NA values

--- R/corrplot-block.R
+++ R/corrplot-block.R
@@ -4,24 +4,25 @@
 #'
 #' @export
 corrplot_block <- function(data, ...) {
-
   fields <- list(
     method = new_select_field("circle", choices= c("circle", "square", "ellipse", "number", "shade", "color", "pie")),
     type = new_select_field("full", choices= c("full", "lower", "upper")),
     order = new_select_field("original",choices= c("original", "AOE", "FPC", "hclust", "alphabet")),
     title = new_string_field()
   )
-
   new_block(
     fields = fields,
     expr = quote({
-      corr_matrix <- stats::cor(data)
+      corr_matrix <- data |> 
+        dplyr::select_if(is.numeric) |> 
+        purrr::discard(~ all(is.na(.))) |>  # removed those column which have all NA Values
+        na.omit() |>  # removed the rows containing na values
+        stats::cor()
       corrplot::corrplot(corr_matrix, method = .(method), type = .(type),
-               order = .(order), addrect = 2, tl.col = "black", tl.srt = 45,
-               diag = FALSE,
-               title = .(title)
-               
-               ) #
+                         order = .(order), addrect = 2, tl.col = "black", tl.srt = 45,
+                         diag = FALSE,
+                         title = .(title)
+      ) #
     }),
     ...,
     class = c("corrplot_block", "plot_block")
